9d606ab4ff41425cef486b347e0275bb504977cb
salt-die <53280662+salt-die@users.noreply.github.com>
2021-04-20 20:41:04 -0500

    Binary operators now defined using classes instead of partial functions.

--- truth_tables/logic_expr.py
+++ truth_tables/logic_expr.py
@@ -1,16 +1,28 @@
-from functools import partial
 from .expr_base import Const, UnOp, BinOp
-
-
-__all__ = 'TRUE', 'FALSE', 'Negate', 'And', 'Or', 'Implies', 'Iff', 'Xor'
 
 TRUE = Const(True)
 FALSE = Const(False)
 
-Negate = partial(UnOp, '~', lambda p: not p)
 
-And = partial(BinOp, 'and', lambda p, q: p and q)
-Or = partial(BinOp, 'or', lambda p, q: p or q)
-Implies = partial(BinOp, '->', lambda p, q: not p or q)
-Iff = partial(BinOp, '<->', lambda p, q: p == q)
-Xor = partial(BinOp, 'xor', lambda p, q: p != q)
+class Negate(UnOp):
+    op = lambda p : not p
+
+
+class And(BinOp):
+    op = lambda p, q: p and q
+
+
+class Or(BinOp):
+    op = lambda p, q: p or q
+
+
+class Implies(BinOp):
+    op = lambda p, q: not p or q
+
+
+class Iff(BinOp):
+    op = lambda p, q: p == q
+
+
+class Xor(BinOp):
+    op = lambda p, q: p != q
