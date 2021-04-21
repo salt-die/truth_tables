0c8148c41e8e0734c1d6f3b471930adf68ab0139
salt-die <53280662+salt-die@users.noreply.github.com>
2021-04-20 20:43:40 -0500

    `binary` parameter added to constructor. `tt` function removed. `show_ast` is now instead a property `ast` that returns a printable string. `display` is now `__str__`. Corrected a bug in the repr involving trailing commas.

--- truth_tables/truth_table.py
+++ truth_tables/truth_table.py
@@ -8,8 +8,9 @@ PARSER = LogicParser()
 
 
 class TruthTable:
-    def __init__(self, *props):
+    def __init__(self, *props, binary=False):
         self.props = props
+        self.binary = binary
 
         self.exprs = exprs = tuple(PARSER.parse(LEXER.tokenize(prop)) for prop in self.props)
         self.vars = vars_ = tuple(sorted(PARSER.vars))
@@ -22,24 +23,25 @@ class TruthTable:
             table.append((*values, *results))
         self.table = tuple(table)
 
-    def display(self, binary=False):
-        translate = '01' if binary else 'FT'
-        values = ((translate[i] for i in row) for row in self.table)
-        table = table_maker(self.vars + self.props, *values)
-        print(table)
+    def __repr__(self):
+        props = "', '".join(self.props)
+        return f"{type(self).__name__}('{props}')"
 
-    def show_ast(self):
-        print(*map(str, self.exprs), sep='\n\n')
+    def __str__(self):
+        translate = '01' if self.binary else 'FT'
+        values = ((translate[i] for i in row) for row in self.table)
+        return table_maker(self.vars + self.props, *values)
+
+    @property
+    def ast(self):
+        """Returns the abstract syntax tree of the propositions as a pretty string.
+        """
+        return '\n\n'.join(map(str, self.exprs))
 
     def __iter__(self):
+        """An iterator over the propositions of the TruthTable.
+        """
         return iter(self.props)
-
-    def __repr__(self):
-        return f'{type(self).__name__}({str(self.props)[1:-2]})'
 
     def __eq__(self, other):
         return self.vars == other.vars and self.table == other.table
-
-
-def tt(*props, binary=False):
-    TruthTable(*props).display(binary)
