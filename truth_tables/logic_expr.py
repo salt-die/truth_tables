996eb9ba0284c7ef5a5a01639a51fb54f78486d4
salt-die <53280662+salt-die@users.noreply.github.com>
2021-04-22 22:16:17 -0500

    Comment no longer valid! Removed!

--- truth_tables/logic_expr.py
+++ truth_tables/logic_expr.py
@@ -7,19 +7,6 @@ FALSE = Const(False)
 class Negate(UnOp):
     op = lambda p : not p
 
-# We must resist the urge to do this:
-# from .q import qMeta
-
-# binops= {
-#     'And'    : lambda p, q: p and q,
-#     'Or'     : lambda p, q: p or q,
-#     'Implies': lambda p, q: not p or q,
-#     'Iff'    : lambda p, q: p == q,
-#     'Xor'    : lambda p, q: p != q,
-# }
-
-# for name, op in binops.items():
-#     globals()[name] = qMeta(name, (BinOp, ), {'op': op, '__fields__': {}})
 
 class And(BinOp):
     op = lambda p, q: p and q
