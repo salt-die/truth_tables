88bdf0e9a692034d29454eebf53809b115440486
salt-die <53280662+salt-die@users.noreply.github.com>
2021-04-22 21:14:03 -0500

    `attrs` renamed to `fields`.
    AutoDict stores fields in a `fields` attribute instead of using `__setitem__`.

--- truth_tables/q.py
+++ truth_tables/q.py
@@ -10,13 +10,13 @@ A simplified version of `https://github.com/salt-die/q`
 """
 class AutoDict(dict):
     def __init__(self):
-        super().__init__(__fields__={})  # Using a dict as an ordered-set
+        self.fields = {}  # Using a dict as an ordered-set
 
     def __missing__(self, key):
         if key.startswith('__'):
             raise KeyError(key)
 
-        self['__fields__'][key] = None
+        self.fields[key] = None
 
 
 class qMeta(type):
@@ -24,21 +24,19 @@ class qMeta(type):
         return AutoDict()
 
     def __new__(meta, name, bases, namespace):
-        attrs = {}
-        for base in bases:
-            attrs |= getattr(base, '__fields__', {})
-        attrs |= namespace['__fields__']
-        namespace['__fields__'] = attrs
+        namespace['__fields__'] = fields = {}
+        for base in reversed(bases):
+            fields |= getattr(base, '__fields__', {})
+        fields |= namespace.fields
 
-        if attrs:
-            init_header = f'def __init__(self, {", ".join(attrs)}):\n'
-            init_body = '\n'.join(f'    self.{attr}={attr}' for attr in attrs)
-            exec(init_header + init_body, namespace)
+        if fields:
+            init_header = f'def __init__(self, {", ".join(fields)}):\n'
+            init_body = '\n'.join(f'    self.{field}={field}' for field in fields)
+            exec(init_header + init_body, globals(), namespace)
 
-        repr_header = 'def __repr__(self):\n'
-        args = ', '.join(f'{{self.{attr}!r}}' for attr in attrs)
-        repr_body = f'    return f"{name}({args})"'
-        exec(repr_header + repr_body, namespace)
+        args = ', '.join(f'{{self.{field}!r}}' for field in fields)
+        repr_ = f'def __repr__(self):\n    return f"{name}({args})"'
+        exec(repr_, globals(), namespace)
 
         return super().__new__(meta, name, bases, namespace)
 
